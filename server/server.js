c9751b6cbc8de886379d59f8fd75eaaf444c68ce
Deep Singnapure <deepsingnapure5@gmail.com>
2026-02-08 16:14:17 +0530

    Add production frontend URL to CORS

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ await connectDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+// const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://greencart-frontend-8rvh.onrender.com'
+]
 
 app.post('/stripe', express.raw({type: 'application/json'}),stripeWebhooks);
 
